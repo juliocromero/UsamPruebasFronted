0a6ac6d2472a0761a962862ab4d796246d1eb205
juliocromerosooft <jromero@sooft.com.ar>
2022-07-25 21:25:55 -0300

    carga proyecto

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+      </v-card>
+      <v-card>
+        <v-card-text>
+          <v-alert
+          type="red"
+          > <p>hola danielito</p></v-alert>
+          <v-btn
+        class="ma-2"
+        text
+        icon
+        color="blue lighten-2"
+      >
+        <v-icon>mdi-thumb-up</v-icon>
+      </v-btn>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
